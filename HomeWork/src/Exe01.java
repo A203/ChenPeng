78068fac2f44d34a19fdcd923f1a01daa58a2f77
hfutchenpeng <454366939@qq.com>
2015-07-08 08:58:31 +0800

    作业
    作业

---
+++ HomeWork/src/Exe01.java
@@ -0,0 +1,9 @@
+
+public class Exe01 {
+	public static void main(String[] args){
+		boolean flag1 = true;
+		boolean flag2 = false;
+		//boolean flag3 = 0;
+		System.out.println(flag1+" "+flag2);
+	}
+}
